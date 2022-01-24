c156857788c8d001b6e162dd0b04120f4980b51b
Luke Hsiao <luke@hsiao.dev>
2022-01-23 16:45:36 -0800

    feat: add basic tracing support

--- task03/src/main.rs
+++ task03/src/main.rs
@@ -1,15 +1,31 @@
 use std::net::SocketAddr;
 
 use axum::{routing::get, Router, Server};
+use tower_http::trace::TraceLayer;
+use tracing::debug;
+use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // Set the RUST_LOG, if it hasn't been explicitly defined
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "task03=debug,tower_http=debug")
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
+    debug!("Listening on {addr}");
     Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler() -> &'static str {
+    debug! {"Responding."};
+    "Hello, World!"
 }
