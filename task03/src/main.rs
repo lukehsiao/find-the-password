c11036cb2f5040f032cf028763f5c2b8d2cd8837
Luke Hsiao <luke@hsiao.dev>
2022-01-23 16:54:18 -0800

    test: add hello world unit tests

--- task03/src/main.rs
+++ task03/src/main.rs
@@ -13,10 +13,7 @@ async fn main() {
     }
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(handler))
-        .layer(TraceLayer::new_for_http());
-
+    let app = app();
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     debug!("Listening on {addr}");
     Server::bind(&addr)
@@ -25,7 +22,58 @@ async fn main() {
         .unwrap();
 }
 
+fn app() -> Router {
+    Router::new()
+        .route("/", get(handler))
+        .layer(TraceLayer::new_for_http())
+}
+
 async fn handler() -> &'static str {
-    debug! {"Responding."};
     "Hello, World!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use tower::util::ServiceExt;
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = app();
+
+        // `Router` implements `tower::Service<Request<Body>>` so we can
+        // call it like any tower service, no need to run an HTTP server.
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn not_found() {
+        let app = app();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/does-not-exist")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert!(body.is_empty());
+    }
 }
