ec55ee3d3ee14454d34582e6e8f0e18c51c8fd8b
Luke Hsiao <luke@hsiao.dev>
2022-01-24 21:28:33 -0800

    feat: add basic user creation

--- task03/src/main.rs
+++ task03/src/main.rs
@@ -5,7 +5,15 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-use axum::{response::Html, routing::get, Router, Server};
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::Html,
+    routing::get,
+    AddExtensionLayer, Json, Router, Server,
+};
+use rand::{distributions::Alphanumeric, Rng};
+use serde::{Deserialize, Serialize};
 use tower_http::trace::TraceLayer;
 use tracing::debug;
 use tracing_subscriber;
@@ -19,14 +27,21 @@ struct State {
     allowed_users: HashSet<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 struct UserState {
     name: String,
     eligible: bool,
     solved: bool,
     hits_before_first_solve: u64,
-    secret: String,
+    #[serde(skip)]
+    secret_idx: usize,
+    #[serde(skip)]
+    passwords: HashSet<String>,
 }
+
+const NUM_PASSWORDS: usize = 20_000;
+
+const PASS_LEN: usize = 32;
 
 #[tokio::main]
 async fn main() {
@@ -48,9 +63,20 @@ async fn main() {
 }
 
 fn app() -> Router {
+    let mut allowed_users: HashSet<String> = HashSet::new();
+    allowed_users.insert("test_user".to_string());
+
+    let shared_state: SharedState = Arc::new(RwLock::new(State {
+        users: HashMap::new(),
+        total_hits: 0,
+        allowed_users,
+    }));
+
     Router::new()
         .route("/03", get(readme))
+        .route("/03/user/:user", get(user_stats).post(create_user))
         .layer(TraceLayer::new_for_http())
+        .layer(AddExtensionLayer::new(shared_state))
 }
 
 /// Provide the README to the root path
@@ -59,30 +85,116 @@ async fn readme() -> Html<&'static str> {
     Html(readme)
 }
 
+async fn create_user(
+    Path(username): Path<String>,
+    Extension(state): Extension<SharedState>,
+) -> Result<Json<UserState>, StatusCode> {
+    if state.read().unwrap().allowed_users.contains(&username) {
+        let mut rng = rand::thread_rng();
+        // Don't want it too close to the front for those who will try to brute force
+        let secret_idx = rng.gen_range(3000..NUM_PASSWORDS);
+
+        let passwords: HashSet<String> = (0..NUM_PASSWORDS)
+            .map(|_| {
+                rng.clone()
+                    .sample_iter(&Alphanumeric)
+                    .take(PASS_LEN)
+                    .map(char::from)
+                    .collect()
+            })
+            .collect();
+
+        let new_user = UserState {
+            name: username,
+            eligible: true,
+            solved: false,
+            hits_before_first_solve: 0,
+            secret_idx,
+            passwords,
+        };
+
+        state
+            .write()
+            .unwrap()
+            .users
+            .insert(String::from(&new_user.name), new_user.clone());
+
+        Ok(Json(new_user))
+    } else {
+        Err(StatusCode::FORBIDDEN)
+    }
+}
+
+async fn user_stats(
+    Path(username): Path<String>,
+    Extension(state): Extension<SharedState>,
+) -> Result<Json<UserState>, StatusCode> {
+    if let Some(user) = state.read().unwrap().users.get(&username) {
+        Ok(Json(user.clone()))
+    } else {
+        Err(StatusCode::NOT_FOUND)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use axum::{
         body::Body,
+        http,
         http::{Request, StatusCode},
     };
+    use mime;
     use tower::util::ServiceExt;
 
     #[tokio::test]
-    async fn hello_world() {
+    async fn root() {
         let app = app();
+
+        let readme = include_str!("../README.html");
 
         // `Router` implements `tower::Service<Request<Body>>` so we can
         // call it like any tower service, no need to run an HTTP server.
         let response = app
-            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .oneshot(Request::builder().uri("/03").body(Body::empty()).unwrap())
             .await
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
 
         let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-        assert_eq!(&body[..], b"Hello, World!");
+        assert_eq!(&body[..], readme.as_bytes());
+    }
+
+    #[tokio::test]
+    async fn create_user() {
+        let app = app();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/03/user/test_user")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let response_body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let user: UserState = serde_json::from_slice(&response_body).unwrap();
+        let gold = UserState {
+            name: String::from("test_user"),
+            eligible: true,
+            solved: false,
+            hits_before_first_solve: 0,
+            secret_idx: 0,
+            passwords: HashSet::new(),
+        };
+        assert_eq!(user, gold);
     }
 
     #[tokio::test]
