1b2324c34bcaf0ba631dad69c74cf2d3ddc01e86
Luke Hsiao <luke@hsiao.dev>
2022-01-26 19:29:58 -0800

    chore(examples): add log statements

--- task03/examples/solution/main.rs
+++ task03/examples/solution/main.rs
@@ -1,16 +1,21 @@
 use std::io::{self, Read};
+use tracing::debug;
 
 use anyhow::Result;
 use rayon::prelude::*;
 
 fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
     let passwords: Vec<String> = input.lines().map(|n| n.trim().to_string()).collect();
 
     let pass = passwords.par_iter().find_any(|n| {
+        let url = format!("https://challenge.hsiao.dev/03/u/luke/check/{n}");
+        debug!(url = %url, "Trying URL");
         let body: String =
-            ureq::get(format!("https://challenge.hsiao.dev/03/u/luke/check/{}", n).as_str())
+            ureq::get(format!("https://challenge.hsiao.dev/03/u/luke/check/{n}").as_str())
                 .call()
                 .unwrap()
                 .into_string()
