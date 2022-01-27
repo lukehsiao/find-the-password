a10f4bc927b88e8ba3765b7af7da6ea3d495b2d2
Luke Hsiao <luke@hsiao.dev>
2022-01-26 19:21:28 -0800

    chore(examples): use the correct URL

--- task03/examples/solution/main.rs
+++ task03/examples/solution/main.rs
@@ -9,11 +9,12 @@ fn main() -> Result<()> {
     let passwords: Vec<String> = input.lines().map(|n| n.trim().to_string()).collect();
 
     let pass = passwords.par_iter().find_any(|n| {
-        let body: String = ureq::get(format!("http://localhost:3000/03/luke/check/{}", n).as_str())
-            .call()
-            .unwrap()
-            .into_string()
-            .unwrap();
+        let body: String =
+            ureq::get(format!("https://challenge.hsiao.dev/03/u/luke/check/{}", n).as_str())
+                .call()
+                .unwrap()
+                .into_string()
+                .unwrap();
         if body.starts_with("True") {
             println!("{}", body);
             true
