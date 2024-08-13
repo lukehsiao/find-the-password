ff58d69e76be8912ec0194efb5a253726bd3a8ee
Luke Hsiao <luke@hsiao.dev>
2024-08-12 21:53:37 -0600

    chore: clean house, initial leptos

--- Justfile
+++ Justfile
@@ -25,7 +25,8 @@ test:
 
 # Sets up a watcher that lints, tests, and builds
 watch:
-	cargo watch -x 'clippy --all-targets --all-features -- -D warnings' -x 'nextest run' -x 'build --release'
+	# cargo watch -x 'clippy --all-targets --all-features -- -D warnings' -x 'nextest run' -x 'build --release' 
+	cargo leptos watch
 
 # Update the changelog using git-cliff
 _update_changelog version:
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,75 @@
-use anyhow::Result;
+use tokio::{self, signal};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use challenges::{
-    config::get_config,
-    http::Application,
-    telemetry::{get_subscriber, init_subscriber},
-};
-
+#[cfg(feature = "ssr")]
 #[tokio::main]
-async fn main() -> Result<()> {
-    // Initialize telemetry
-    let subscriber = get_subscriber("challenges".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
+async fn main() {
+    // Enable tracing.
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "code-challenges=debug,tower_http=debug,axum=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    // Grab our configuration parameters
-    let config = get_config()?;
+    use axum::Router;
+    use code_challenges::app::*;
+    use leptos::prelude::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
 
-    let application = Application::build(config).await?;
-    application.run_until_stopped().await?;
-    Ok(())
+    let conf = get_configuration(None).unwrap();
+    let addr = conf.leptos_options.site_addr;
+    let leptos_options = conf.leptos_options;
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(App);
+
+    let app = Router::new()
+        .leptos_routes(&leptos_options, routes, {
+            let leptos_options = leptos_options.clone();
+            move || shell(leptos_options.clone())
+        })
+        .fallback(leptos_axum::file_and_error_handler(shell))
+        .with_state(leptos_options);
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    log!("listening on http://{}", &addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    // Run the server with graceful shutdown
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+#[cfg(not(feature = "ssr"))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
