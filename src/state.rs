939dce477946d56e555ba74fa8fe5a5f4bb691db
Luke Hsiao <luke@hsiao.dev>
2024-08-16 22:13:44 -0600

    refactor: show time to solve and attempts to solve in leaderboard
    This also fixes the styling.

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,12 @@
 use axum::extract::FromRef;
-use jiff::Timestamp;
 use leptos::LeptosOptions;
 use std::sync::Arc;
 
-use crate::user::UserMap;
+use crate::user::{Completion, UserMap};
 
 #[derive(FromRef, Debug, Clone)]
 pub struct AppState {
     pub leptos_options: LeptosOptions,
     pub usermap: Arc<UserMap>,
-    pub leaderboard: Arc<Vec<(String, Timestamp)>>,
+    pub leaderboard: Arc<Vec<Completion>>,
 }
